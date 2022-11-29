72fbf47b00dbc0c9601697c52d7dbb65bb3c88e6
admirerbrown <admirerbrown@gmail.com>
2022-11-29 07:50:52 +0000

    preliminary version

--- index.js
+++ index.js
@@ -1,54 +1,45 @@
+const addBook = document.querySelector(".add-btn");
+const formVal = document.querySelectorAll("form input");
+let collections = [];
+const bookshelf = document.querySelector(".bookshelf");
 
+function showBooks(title, author, counter) {
+     const div = document.createElement('div');
+     div.id = counter;
+     
+  div.innerHTML = `
+            <p>${title}</p>
+            <p>${author}</p>
+            <button class="remove ${counter}">Remove</button>
+            <hr>`;
+            bookshelf.appendChild(div);
 
-
-
-
-
-
-
-let collections = [
-    {
-        title: 'book1',
-        author: 'John',
-    },
-    {
-        title: 'book2',
-        author: 'pope',
-    }
-];
-
-const bookPlate = {
-    title: 'paul in jail',
-    author: 'timothy',
+  const takeout = document.querySelectorAll(".remove");
+  takeout.forEach((item) => {
+    item.addEventListener("click", (item) =>{
+        // console.log(item)
+        const bookList = document.querySelectorAll('.div');
+        div.parentNode.removeChild(div);
+        localStorage.setItem('books', JSON.stringify(collections));
+    })
+  })
 }
 
-// add book.
-function addBook(title, author) {
-    const book = Object.create(bookPlate);
-    book.title = title;
-    book.author = author;
-    collections.push(book);
+let counter = 0;
+addBook.addEventListener("click", (e) => {
+  e.preventDefault();
+  counter += 1;
+  const titleVal = formVal[0].value;
+  const authorVal = formVal[1].value;
+  collections.push({ titleVal, authorVal, counter});
+  console.log("i am ", collections);
+
+  // store bookdata
+  localStorage.setItem("books", JSON.stringify(collections));
+  showBooks(titleVal, authorVal, counter);
+});
+
+if (localStorage.getItem('books')){
+    bookList = JSON.parse(localStorage.getItem('books'));
+   console.log(bookList);
 }
-
-console.log(collections);
-
-// add a new book to the store
-addBook('new moon', 'dracula');
-console.log( "book added is ", collections);
-console.log("book has been deleted", collections);
-
-// remove book.
-function removeBook( yourTitle) {
-    const titleToRemove = yourTitle;
-    const filteredbooks = collections.filter((item) => item.title !== titleToRemove);
-    collections = filteredbooks;
-}
-
-
-// store b
-localStorage.setItem('wholeBookList', JSON.stringify(collections));
-const getBookList = localStorage.getItem('wholeBookList');
-const finallist = JSON.parse(getBookList);
-
-console.log( "hey i am finalList:", finallist);
-
