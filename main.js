9d8039e0b75d04d6b5cc8d0742c72aa5f623603b
Anna Petras <apetras2017@gmail.com>
2022-11-29 15:20:14 +0100

    refactor title and author add in javascript

--- main.js
+++ main.js
@@ -1,62 +1,45 @@
-/* eslint-disable no-unused-vars */
-
-const Title = document.querySelector(".title");
-const author = document.querySelector(".author");
-const registeredBooks = document.querySelector(".book-list");
-const addButton = document.querySelector(".addbook");
+const Title = document.querySelector('.title');
+const author = document.querySelector('.author');
+const registeredBooks = document.querySelector('.book-list');
+const addButton = document.querySelector('.addbook');
 
 let Books = [];
 
-
-class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
+function BookData() {
+  registeredBooks.innerHTML = '';
+  for (let i = 0; i < Books.length; i++) {
+    registeredBooks.innerHTML += `
+    <div>
+      <p class="Title">${Books[i].Title}</p>
+      <p class="author">${Books[i].author}</p>
+      <button class="button" onclick="remove(${i})">remove</button>
+      <hr/>
+    </div>
+   `;
+    Title.value = '';
+    author.value = '';
   }
 }
 
-//UI class
-
-class UI {
-  static addBooks() {
-    registeredBooks.innerHTML = "";
-    for (let i = 0; i < Books.length; i++) {
-      registeredBooks.innerHTML += `
-      <div class="title-and-author"> 
-        <p class="Title">"${Books[i].title}" by  ${Books[i].author}</p>
-        <button class="button" onclick="UI.remove(${i})">remove</button>
-      </div>
-
-     `;
-      Title.value = "";
-      author.value = "";
-      Title.focus();
-    }
-  }
-//remove
-
-  static remove(index) {
-    Books.splice(index, 1);
-    UI.addBooks();
-    localStorage.setItem("Books", JSON.stringify(Books));
-  }
-  
+function remove(index) {
+  Books.splice(index, 1);
+  BookData();
+  localStorage.setItem('Books', JSON.stringify(Books));
 }
 
 window.onload = () => {
-  if (localStorage.getItem("Books")) {
-    Books = JSON.parse(localStorage.getItem("Books"));
+  if (localStorage.getItem('Books')) {
+    Books = JSON.parse(localStorage.getItem('Books'));
   }
-  UI.addBooks();
+  BookData();
 };
 
-//add
-
-addButton.addEventListener("click", () => {
-  const book = new Book(Title.value, author.value);
+addButton.addEventListener('click', () => {
+  const book = {
+    Title: Title.value,
+    author: author.value,
+  };
   Books.push(book);
-  UI.addBooks();
-  localStorage.setItem("Books", JSON.stringify(Books));
+  BookData();
+  localStorage.setItem('Books', JSON.stringify(Books));
 });
-
-
